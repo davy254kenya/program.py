1f85d6eab7c2fe86c2e860c4d28a821119f8b141
davy254kenya <160719755+davy254kenya@users.noreply.github.com>
2024-03-25 18:32:47 +0300

    Add files via upload

---
+++ program.py
@@ -0,0 +1,10 @@
+# user_input.py
+
+# Ask the user for their name
+name = input("What is your name? ")
+# Ask the user for their age
+age = input("How old are you? ")
+# Ask the user for their location
+location = input("Where do you live? ")
+# Print a personalized message
+print("Hello my name is david, 21 years old and I live in Nairobi.")
